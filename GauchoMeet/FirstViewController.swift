4f8aa7d184f89c962666cb04daffe79621d8f518
tyler@mbp <ouyang@cs.ucsb.edu>
2015-07-30 23:33:15 -0700

    Project setup

---
+++ GauchoMeet/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GauchoMeet
+//
+//  Created by Tyler Weimin Ouyang on 7/30/15.
+//  Copyright © 2015 Golden. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
